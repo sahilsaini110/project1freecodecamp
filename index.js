a6ec4190ac196bc7082ee8f8c038fc5a1aec580c
Sahil <sohil170246@gmail.com>
2022-06-26 11:46:56 +0530

    model created.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,15 +17,21 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+  
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let date_string = req.params.date_string;
+  let date = new Date(date_string);
+  if(date == "Invalid Date"){
+    res.json({"error": "Invalid Date"});
+  } else {
+    res.json({unix: date.getTime(), utc: date.toGMTString() });
+  }
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
